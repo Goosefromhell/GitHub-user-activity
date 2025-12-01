6133945aa8ce8d889e2c5ea0b2c14b1323b49c75
Fiction <goosefromhellyes@gmail.com>
2025-12-01 14:06:33 +0100

    fix pullrequest logic

--- src/main/java/org/govnorgatization/githubuseractivity/Main.java
+++ src/main/java/org/govnorgatization/githubuseractivity/Main.java
@@ -21,86 +21,96 @@ public class Main {
 
         Path target = Path.of(System.getProperty("user.home"), "Buffers", "GitHubUserActivity", "activity.json");
 
-            if(args.length != 1) {
-                System.out.println("Write user name");
-            }else{
+        if (args.length != 1) {
+            System.out.println("Write user name");
+        } else {
 
-                try {
-
-
-                    String user = String.format("https://api.github.com/users/%s/events", args[0]);
-
-                    HttpClient client = HttpClient.newHttpClient();
-                    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(user)).build();
-                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    if (response.statusCode() == 404) {
-                        System.out.println("User is not found");
-                    } else if (response.statusCode() != 200 && response.statusCode() != 404) {
-                        System.out.println("Error occurred: " + response.statusCode());
-                    } else {
+            try {
 
 
-                        ObjectMapper mapper = new ObjectMapper();
+                String user = String.format("https://api.github.com/users/%s/events", args[0]);
 
-                        Files.createDirectories(target.getParent());
-
-                        JsonNode json = mapper.readTree(response.body());
-                        mapper.writerWithDefaultPrettyPrinter().writeValue(target.toFile(), json);
-                        JsonNode jsonNode = mapper.readTree(target.toFile());
-
-                        HashMap<String, Integer> amount_of_pushes = new HashMap<>();
-                        ArrayList<String> repos_created = new ArrayList<>();
-                        HashMap<String, Integer> amount_of_pull_requests = new HashMap<>();
-                        HashMap<String, Integer> amount_of_pull_requests_reviews = new HashMap<>();
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(user)).build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 404) {
+                    System.out.println("User is not found");
+                } else if (response.statusCode() != 200 && response.statusCode() != 404) {
+                    System.out.println("Error occurred: " + response.statusCode());
+                } else {
 
 
-                        for (JsonNode kal : jsonNode) {
-                            if (kal.get("type").asString().equals("PushEvent")) {
-                                String repo_name = kal.get("repo").get("name").asString();
-                                amount_of_pushes.put(repo_name, amount_of_pushes.getOrDefault(repo_name, 0) + 1);
-                            } else if (kal.get("type").asString().equals("CreateEvent")) {
-                                repos_created.add(kal.get("repo").get("name").asString());
-                            } else if (kal.get("type").asString().equals("PullRequestEvent")) {
-                                String repo_name = kal.get("repo").get("name").asString();
-                                amount_of_pull_requests.put(repo_name, amount_of_pull_requests.getOrDefault(repo_name, 0) + 1);
+                    ObjectMapper mapper = new ObjectMapper();
 
-                            } else if (kal.get("type").asString().equals("PullRequestReviewEvent")) {
-                                String repo_name = kal.get("repo").get("name").asString();
-                                amount_of_pull_requests_reviews.put(repo_name, amount_of_pull_requests_reviews.getOrDefault(repo_name, 0) + 1);
+                    Files.createDirectories(target.getParent());
 
-                            }
-                            else if (kal.get("type").asString().equals("IssuesEvent")) {
-                                switch (kal.get("payload").get("action").asString()){
-                                    case "opened" -> System.out.printf("Opened new issue in %s \n", kal.get("repo").get("name"));
-                                    case "closed" -> System.out.printf("Closed  issue in %s \n", kal.get("repo").get("name"));
-                                    case "reopened" -> System.out.printf("Reopened  issue in %s \n", kal.get("repo").get("name"));
-                                    default -> System.out.println("Unknown action");
-                                }
+                    JsonNode json = mapper.readTree(response.body());
+                    mapper.writerWithDefaultPrettyPrinter().writeValue(target.toFile(), json);
+                    JsonNode jsonNode = mapper.readTree(target.toFile());
+
+                    HashMap<String, Integer> amount_of_pushes = new HashMap<>();
+                    ArrayList<String> repos_created = new ArrayList<>();
+                    HashMap<String, Integer> amount_of_pull_requests = new HashMap<>();
+                    HashMap<String, Integer> amount_of_pull_requests_reviews = new HashMap<>();
+
+
+                    for (JsonNode kal : jsonNode) {
+                        if (kal.get("type").asString().equals("PushEvent")) {
+                            String repo_name = kal.get("repo").get("name").asString();
+                            amount_of_pushes.put(repo_name, amount_of_pushes.getOrDefault(repo_name, 0) + 1);
+                        } else if (kal.get("type").asString().equals("CreateEvent")) {
+                            repos_created.add(kal.get("repo").get("name").asString());
+                        } else if (kal.get("type").asString().equals("PullRequestEvent")) {
+                            switch (kal.get("payload").get("action").asString()) {
+                                case "opened" ->
+                                        System.out.printf("Opened new pullrequest in %s \n", kal.get("repo").get("name"));
+                                case "closed" ->
+                                        System.out.printf("Closed pullrequest in %s \n", kal.get("repo").get("name"));
+                                case "reopened" ->
+                                        System.out.printf("Reopened  pullrequest in %s \n", kal.get("repo").get("name"));
+                                default -> System.out.println("Unknown action");
 
                             }
 
-                        }
+                        } else if (kal.get("type").asString().equals("PullRequestReviewEvent")) {
+                            String repo_name = kal.get("repo").get("name").asString();
+                            amount_of_pull_requests_reviews.put(repo_name, amount_of_pull_requests_reviews.getOrDefault(repo_name, 0) + 1);
 
-                        for (Map.Entry<String, Integer> entry : amount_of_pushes.entrySet()) {
-                            System.out.printf("User pushed %d commits to %s \n", entry.getValue(), entry.getKey());
-                        }
-                        for (String repo_name : repos_created) {
-                            System.out.printf("Started %s \n", repo_name);
-                        }
-                        for (Map.Entry<String, Integer> entry : amount_of_pull_requests.entrySet()) {
-                            System.out.printf("Pull requests opened %d in %s \n", entry.getValue(), entry.getKey());
-                        }
+                        } else if (kal.get("type").asString().equals("IssuesEvent")) {
+                            switch (kal.get("payload").get("action").asString()) {
+                                case "opened" ->
+                                        System.out.printf("Opened new issue in %s \n", kal.get("repo").get("name"));
+                                case "closed" ->
+                                        System.out.printf("Closed  issue in %s \n", kal.get("repo").get("name"));
+                                case "reopened" ->
+                                        System.out.printf("Reopened  issue in %s \n", kal.get("repo").get("name"));
+                                default -> System.out.println("Unknown action");
+                            }
 
-                        for (Map.Entry<String, Integer> entry : amount_of_pull_requests_reviews.entrySet()) {
-                            System.out.printf("Reviewed %d requests in %s repositories \n", entry.getValue(), entry.getKey());
                         }
 
                     }
-                } catch (InterruptedException | IOException e) {
-                    System.out.println("Error occurred:" + e);
+
+                    for (Map.Entry<String, Integer> entry : amount_of_pushes.entrySet()) {
+                        System.out.printf("User pushed %d commits to %s \n", entry.getValue(), entry.getKey());
+                    }
+                    for (String repo_name : repos_created) {
+                        System.out.printf("Started %s \n", repo_name);
+                    }
+//                        for (Map.Entry<String, Integer> entry : amount_of_pull_requests.entrySet()) {
+//                            System.out.printf("Pull requests opened %d in %s \n", entry.getValue(), entry.getKey());
+//                        }
+
+                    for (Map.Entry<String, Integer> entry : amount_of_pull_requests_reviews.entrySet()) {
+                        System.out.printf("Reviewed %d requests in %s repositories \n", entry.getValue(), entry.getKey());
+                    }
 
                 }
+            } catch (InterruptedException | IOException e) {
+                System.out.println("Error occurred:" + e);
+
             }
+        }
 
 
     }
